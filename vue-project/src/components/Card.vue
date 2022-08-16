4bf980758bd175a79d54917b46c14013e45e0218
B10756052 <B10756052@gmail.com>
2022-08-16 10:00:27 +0800

    外觀微調

--- vue-project/src/components/Card.vue
+++ vue-project/src/components/Card.vue
@@ -9,7 +9,7 @@
             <div class="col-4 col-md-3 d-flex" style="padding:0;">
                 <img src="https://picsum.photos/150" class="rounded-start p-2 w-100  " alt="..." style="min-width:144px">
              </div>
-            <div class="col-8 col-md-9 m-0 p-0">
+            <div class="col-8 col-md-9 m-0 px-2 px-sm-0 py-md-2">
                 <div div class="card-body px-md-0  px-4 m-1" style="margin-top:0.5rem;">
                  <h5 class="card-title">User name</h5>
                     <p class="card-text  text-truncate" style="margin:0;">This s is a little bs a little bit longs a little bit longit longerThis s is a little bit longer..</p>
@@ -24,7 +24,7 @@
             <div class="col-4 col-md-3 d-flex" style="padding:0;">
                 <img src="https://picsum.photos/150" class="rounded-start p-2 w-100  " alt="..." style="min-width:144px">
              </div>
-            <div class="col-8 col-md-9 m-0 p-0">
+            <div class="col-8 col-md-9 m-0 px-2 px-sm-0 py-md-2">
                 <div div class="card-body px-md-0  px-4 m-1" style="margin-top:0.5rem;">
                  <h5 class="card-title">User name</h5>
                     <p class="card-text  text-truncate" style="margin:0;">This s is a little bs a little bit longs a little bit longit longerThis s is a little bit longer..</p>
@@ -39,7 +39,7 @@
             <div class="col-4 col-md-3 d-flex" style="padding:0;">
                 <img src="https://picsum.photos/150" class="rounded-start p-2 w-100  " alt="..." style="min-width:144px">
              </div>
-            <div class="col-8 col-md-9 m-0 p-0">
+            <div class="col-8 col-md-9 m-0 px-2 px-sm-0 py-md-2">
                 <div div class="card-body px-md-0  px-4 m-1" style="margin-top:0.5rem;">
                  <h5 class="card-title">User name</h5>
                     <p class="card-text  text-truncate" style="margin:0;">This s is a little bs a little bit longs a little bit longit longerThis s is a little bit longer..</p>
@@ -54,7 +54,7 @@
             <div class="col-4 col-md-3 d-flex" style="padding:0;">
                 <img src="https://picsum.photos/150" class="rounded-start p-2 w-100  " alt="..." style="min-width:144px">
              </div>
-            <div class="col-8 col-md-9 m-0 p-0">
+            <div class="col-8 col-md-9 m-0 px-2 px-sm-0 py-md-2">
                 <div div class="card-body px-md-0  px-4 m-1" style="margin-top:0.5rem;">
                  <h5 class="card-title">User name</h5>
                     <p class="card-text  text-truncate" style="margin:0;">This s is a little bs a little bit longs a little bit longit longerThis s is a little bit longer..</p>
@@ -69,7 +69,7 @@
             <div class="col-4 col-md-3 d-flex" style="padding:0;">
                 <img src="https://picsum.photos/150" class="rounded-start p-2 w-100  " alt="..." style="min-width:144px">
              </div>
-            <div class="col-8 col-md-9 m-0 p-0">
+            <div class="col-8 col-md-9 m-0 px-2 px-sm-0 py-md-2">
                 <div div class="card-body px-md-0  px-4 m-1" style="margin-top:0.5rem;">
                  <h5 class="card-title">User name</h5>
                     <p class="card-text  text-truncate" style="margin:0;">This s is a little bs a little bit longs a little bit longit longerThis s is a little bit longer..</p>
@@ -79,7 +79,9 @@
             </div>
         </div>
     </div>
+         
     
+       
      
   </main>
 </template>
